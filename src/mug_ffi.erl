ee3bba35fe733d22175d9a81138c8985c4e538b8
Sakari Bergen <s@beatwaves.net>
2025-05-31 18:16:56 +0300

    Update to newer stdlib, gleam_erlang, and glisten

--- src/mug_ffi.erl
+++ src/mug_ffi.erl
@@ -1,6 +1,6 @@
 -module(mug_ffi).
 
--export([send/2, shutdown/1, coerce/1]).
+-export([send/2, shutdown/1, coerce_tcp_message/1]).
 
 send(Socket, Packet) ->
     normalise(gen_tcp:send(Socket, Packet)).
@@ -8,9 +8,18 @@ send(Socket, Packet) ->
 shutdown(Socket) ->
     normalise(gen_tcp:shutdown(Socket, read_write)).
 
-normalise(ok) -> {ok, nil};
-normalise({ok, T}) -> {ok, T};
-normalise({error, {timeout, _}}) -> {error, timeout};
-normalise({error, _} = E) -> E.
+normalise(ok) ->
+    {ok, nil};
+normalise({ok, T}) ->
+    {ok, T};
+normalise({error, {timeout, _}}) ->
+    {error, timeout};
+normalise({error, _} = E) ->
+    E.
 
-coerce(T) -> T.
+coerce_tcp_message({tcp, Socket, Data}) ->
+    {packet, Socket, Data};
+coerce_tcp_message({tcp_closed, Socket}) ->
+    {socket_closed, Socket};
+coerce_tcp_message({tcp_error, Socket, Error}) ->
+    {tcp_error, Socket, Error}.
