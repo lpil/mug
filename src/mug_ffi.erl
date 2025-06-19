4fd0c0affef6e55d0902ba1f15af1b7a6e23c3be
Louis Pilfold <louis@lpil.uk>
2025-06-19 16:13:32 +0100

    Update for latest

--- src/mug_ffi.erl
+++ src/mug_ffi.erl
@@ -1,6 +1,12 @@
 -module(mug_ffi).
 
--export([send/2, shutdown/1, coerce_tcp_message/1]).
+-export([send/2, shutdown/1, coerce_tcp_message/1, active_once/0, passive/0]).
+
+active_once() ->
+    once.
+
+passive() ->
+    false.
 
 send(Socket, Packet) ->
     normalise(gen_tcp:send(Socket, Packet)).
